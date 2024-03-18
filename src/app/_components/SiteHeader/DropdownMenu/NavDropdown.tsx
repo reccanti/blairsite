dfe4e6b920a78329ae6570d74b8c7deeaa084b73
Blair Wilcox <reccanti@gmail.com>
2024-03-18 16:58:06 -0400

    See if adding regular "a" tags works
    See if replacing next links with "a" tags fixes the scrolling issue

--- src/app/_components/SiteHeader/DropdownMenu/NavDropdown.tsx
+++ src/app/_components/SiteHeader/DropdownMenu/NavDropdown.tsx
@@ -1,10 +1,16 @@
+import cx from "classnames";
 import { Button } from "@/components/Button";
 import { DropdownMenu, Menu, Item } from "./DropdownMenu";
 import { DropdownMenuToggleButton } from "./ToggleButton";
 import MenuIcon from "@/components/Icons/MenuIcon";
+import { button } from "@/components/Button/Button.css";
 import { itemButton } from "./DropdownMenu.css";
+import { semanticColorKeymap } from "@/themes/sprinkles/colors.css";
 
 export function NavDropdown() {
+  const colorButton = (color: keyof typeof semanticColorKeymap) =>
+    cx(itemButton, button({ color, hasPadding: true }));
+
   return (
     <DropdownMenu
       toggleButton={
@@ -17,44 +23,24 @@ export function NavDropdown() {
     >
       <Menu>
         <Item>
-          <Button
-            className={itemButton}
-            href="/#about"
-            color="Red"
-            scroll={false}
-          >
+          <a className={colorButton("Red")} href="/#about" tabIndex={0}>
             Who&#39;s This Gal?
-          </Button>
+          </a>
         </Item>
         <Item>
-          <Button
-            className={itemButton}
-            href="/#projects"
-            color="Yellow"
-            scroll={false}
-          >
+          <a className={colorButton("Yellow")} href="/#projects" tabIndex={0}>
             Cool Stuff I&#39;ve Worked On
-          </Button>
+          </a>
         </Item>
         <Item>
-          <Button
-            className={itemButton}
-            href="/#skills"
-            color="Green"
-            scroll={false}
-          >
+          <a className={colorButton("Green")} href="/#skills" tabIndex={0}>
             Things I&#39;ve Used
-          </Button>
+          </a>
         </Item>
         <Item>
-          <Button
-            className={itemButton}
-            href="/#contact"
-            color="Blue"
-            scroll={false}
-          >
+          <a className={colorButton("Blue")} href="/#contact" tabIndex={0}>
             Wanna Reach Out?
-          </Button>
+          </a>
         </Item>
         <Item>
           <Button className={itemButton} href="/resume" color="Magenta">
